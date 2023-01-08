5baeb5c49d6c15064ade9f2b45c10f2b07254f76
SYXZ <49202777+SYXZyt@users.noreply.github.com>
2023-01-08 23:55:43 +0000

    Implemented map editor

---
+++ MapEditor/Editor.cs
@@ -0,0 +1,194 @@
+﻿namespace MapEditor
+{
+    internal struct Vector2
+    {
+        public int x;
+        public int y;
+
+        public Vector2() => x = y = 0;
+        public Vector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    internal sealed class Editor
+    {
+        private readonly string fname;
+        private int tileIndex;
+        private readonly int[,] map;
+
+        private Vector2 cursorLast;
+        private Vector2 cursor;
+
+        private readonly char[] tiles = new char[] { '▓', '▒', '╠', '╔', '╦', '╗', '╣', '╝', '╩', '╚', '└', '┘', '┌', '┐' };
+
+        private void InitDraw()
+        {
+            Console.ResetColor();
+            Console.SetCursorPosition(0, 0);
+
+            char[] buffer = new char[(42 * 48) + 42 /* <-- remember to allocate space for the \n*/];
+            int index = 0;
+            for (int y = 0; y < 42; y++)
+            {
+                for (int x = 0; x < 48; x++)
+                {
+                    buffer[index] = tiles[map[x, y]];
+                    index++;
+                }
+                buffer[index] = '\n';
+                index++;
+            }
+            Console.Write(buffer);
+
+            Console.SetCursorPosition(cursor.x, cursor.y);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write(tiles[map[cursor.x, cursor.y]]);
+
+            DrawTiles();
+        }
+
+        private void CleanCursor()
+        {
+            Console.ResetColor();
+            Console.SetCursorPosition(cursorLast.x, cursorLast.y);
+            Console.Write(tiles[map[cursorLast.x, cursorLast.y]]);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(cursor.x, cursor.y);
+            Console.Write(tiles[map[cursor.x, cursor.y]]);
+        }
+
+        private void DrawTiles()
+        {
+            Console.SetCursorPosition(0, 44);
+            Console.ResetColor();
+
+            foreach (char c in tiles)
+            {
+                if (c == tiles[tileIndex])
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+
+                Console.Write(c);
+                Console.ResetColor();
+                Console.Write(' ');
+            }
+        }
+
+        private void UpdateInput()
+        {
+            Console.SetCursorPosition(0, 45);
+            Console.ForegroundColor = Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.CursorVisible = false;
+            ConsoleKey key = Console.ReadKey().Key;
+
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    if (cursor.x == 0) break;
+                    cursorLast = cursor;
+                    cursor.x--;
+                    CleanCursor();
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (cursor.x == 47) break;
+                    cursorLast = cursor;
+                    cursor.x++;
+                    CleanCursor();
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (cursor.y == 0) break;
+                    cursorLast = cursor;
+                    cursor.y--;
+                    CleanCursor();
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (cursor.y == 41) break;
+                    cursorLast = cursor;
+                    cursor.y++;
+                    CleanCursor();
+                    break;
+                case ConsoleKey.Enter:
+                    map[cursor.x, cursor.y] = tileIndex;
+                    CleanCursor();
+                    break;
+                case ConsoleKey.OemPlus:
+                    if (tileIndex < tiles.Length - 1) tileIndex++;
+                    DrawTiles();
+                    break;
+                case ConsoleKey.OemMinus:
+                    if (tileIndex > 0) tileIndex--;
+                    DrawTiles();
+                    break;
+                case ConsoleKey.S:
+                    Save();
+                    break;
+                case ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Save()
+        {
+            using StreamWriter writer = new(fname);
+            for (int y = 0; y < 42; y++)
+            {
+                for (int x = 0; x < 48; x++)
+                {
+                    writer.Write(map[x, y]);
+                } writer.Write("\r\n");
+            }
+
+            writer.Close();
+        }
+
+        public void Run()
+        {
+            InitDraw();
+
+            while (true)
+            {
+                UpdateInput();
+            }
+        }
+
+        public Editor(string fname, bool isNewFile)
+        {
+            this.fname = fname;
+            cursor = new();
+
+            Console.BufferWidth = Console.WindowWidth;
+            Console.BufferHeight = Console.WindowHeight;
+
+            if (isNewFile)
+            {
+                map = new int[48, 42];
+            }
+            else
+            {
+                using StreamReader reader = new(fname);
+                map = new int[48, 42];
+
+                for (int i = 0; i < 42; i++)
+                {
+                    string line = reader.ReadLine();
+                    char[] cs = line.ToCharArray();
+                    for (int j = 0; j < 48; j++) map[j, i] = int.Parse(cs[j].ToString());
+                }
+
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ MapEditor/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace MapEditor
+{
+    internal static class Program
+    {
+        private static int menuOption = 0;
+
+        private delegate void MenuOption();
+        private static MenuOption[] menuOptions = new MenuOption[3] { New, Load, Exit };
+
+        private static void New()
+        {
+            Console.CursorVisible = true;
+            Console.Clear();
+            Console.WriteLine("Enter map name");
+            string map = Console.ReadLine() + ".map";
+
+            Editor editor = new(map, true);
+            Console.WriteLine("Loading!");
+            editor.Run();
+
+            Environment.Exit(0);
+        }
+
+        private static void Load()
+        {
+            Console.CursorVisible = true;
+            Console.Clear();
+            Console.WriteLine("Enter map to load");
+            string map = Console.ReadLine() + ".map";
+
+            bool isNew = !File.Exists(map);
+            Editor editor = new(map, isNew);
+            Console.WriteLine("Loading!");
+            editor.Run();
+
+            Environment.Exit(0);
+        }
+
+        private static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        private static void UpdateMenu()
+        {
+            ConsoleKey key = Console.ReadKey().Key;
+
+            if (key == ConsoleKey.UpArrow)
+            {
+                menuOption--;
+
+                if (menuOption == -1) menuOption = 2;
+            }
+            else if (key == ConsoleKey.DownArrow)
+            {
+                menuOption++;
+
+                if (menuOption > 2) menuOption = 0;
+            }
+            else if (key == ConsoleKey.Enter)
+            {
+                Console.ResetColor();
+                menuOptions[menuOption].Invoke();
+            }
+        }
+
+        private static void DrawMenu()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string[] names = new string[3] { "NEW", "LOAD", "EXIT" };
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (menuOption == i)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+
+                Console.WriteLine(names[i]);
+                Console.ResetColor();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        private static void Main()
+        {
+            AppDomain domain = AppDomain.CurrentDomain;
+            domain.ProcessExit += Domain_ProcessExit;
+
+            Console.Clear();
+
+            /* Menu Options are
+             * NEW
+             * LOAD
+             * EXIT
+             */
+
+            while (true)
+            {
+                Console.CursorVisible = false;
+                DrawMenu();
+                UpdateMenu();
+            }
+        }
+
+        private static void Domain_ProcessExit(object sender, EventArgs e)
+        {
+            Console.ResetColor();
+        }
+    }
+}
